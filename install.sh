7470e7a2f4c01ca22544035370cb0356d29f29a3
Aaron Kuehler <aaron.kuehler@gmail.com>
2020-11-15 15:57:24 -0500

    Make install script more tolerant of existing dependencies

--- install.sh
+++ install.sh
@@ -2,50 +2,70 @@
 # Author: Aaron Kuehler
 # Purpose: Install the dotfiles
 
-DOTFILES_HOME=$HOME/.files
-PLATFORM=$(uname)
+DOTFILES_HOME="$HOME/.files"
+PLATFORM="$(uname)"
 
 function ensure_package_manager() {
-    if [ $PLATFORM == "Darwin" ] && ! [ -x "$(command -v brew)" ]
+    if [ "$PLATFORM" == "Darwin" ] && ! [ -x "$(command -v brew)" ]
     then
         echo "Installing Homebrew..."
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         brew update
-    elif [ $PLATFORM == "Linux" ]
+    elif [ "$PLATFORM" == "Linux" ]
     then
         sudo apt update
     fi
 }
 
-function ensure_dependencies() {
-    local deps="emacs git"
-    if [ $PLATFORM == "Darwin" ]
-    then
-        brew install $deps
-    elif [ $PLATFORM == "Linux" ]
-    then
-        sudo apt install -y $deps
+function ensure_macos_package() {
+    local package
+    package="$1"
+
+    if ! command -v "$package" &> /dev/null
+        then
+            brew install "$package"
     fi
 }
 
-function ensure_latest_dotfiles() {
-    if [ -e $DOTFILES_HOME ]
+function ensure_macos_package() {
+    local package
+    package="$1"
+
+    if ! command -v "$package" &> /dev/null
     then
-        echo "Updating $DOTFILES_HOME"
-        cd $DOTFILES_HOME
-        git pull
-    else
+        sudo apt install -y "$package"
+    fi
+}
+
+
+
+function ensure_dependencies() {
+    if [ "$PLATFORM" == "Darwin" ]
+    then
+        ensure_macos_package "emacs"
+        ensure_macos_package "git"
+    elif [ "$PLATFORM" == "Linux" ]
+    then
+        ensure_apt_package "emacs"
+        ensure_apt_package "git"
+    fi
+}
+
+function ensure_dotfiles() {
+    if [ ! -e "$DOTFILES_HOME" ]
+    then
         echo "Installing dotfiles to: $DOTFILES_HOME"
-        git clone git@github.com:indiebrain/.files.git $DOTFILES_HOME
-        cd $DOTFILES_HOME
+        git clone git@github.com:indiebrain/.files.git "$DOTFILES_HOME"
+        cd "$DOTFILES_HOME" || (echo "Failed to change directory to '$DOTFILES_HOME'" &&  exit 1)
     fi
 }
 
 function tangle_files() {
-    DIR=`pwd`
+    cd "$DOTFILES_HOME" || (echo "Failed to change directory to '$DOTFILES_HOME'" &&  exit 1)
     FILES=""
 
-    for file in `ls -1 | grep \.org | grep -v README.org`; do
+    for file in $(ls -1 ./*.org)
+    do
         FILES="$FILES \"$file\""
     done
 
@@ -55,20 +75,21 @@ function tangle_files() {
           "(progn
             (require 'org)(require 'ob)(require 'ob-tangle)
              (mapc (lambda (file)
-                     (find-file (expand-file-name file \"$DIR\"))
+                     (find-file (expand-file-name file \"$DOTFILES_HOME\"))
                      (org-babel-tangle)
                      (kill-buffer)) '($FILES)))"
 }
 
 function make_setup_executable() {
     local setup_script_path="$DOTFILES_HOME/setup.sh"
+    setup_script_path="$DOTFILES_HOME/setup.sh"
 
-    chmod u+x $setup_script_path
+    chmod u+x "$setup_script_path"
 }
 
 ensure_package_manager
 ensure_dependencies
-ensure_latest_dotfiles
+ensure_dotfiles
 tangle_files
 make_setup_executable
 
