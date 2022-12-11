56b807541ebd8420fb42a89a4db9f41eb6d2c05f
Aaron Kuehler <aaron.kuehler@gmail.com>
2022-12-11 11:06:12 -0500

    Emacs: Add custom initialization

---
+++ emacs/.emacs.d/indiebrain-lisp/indiebrain-common.el
@@ -0,0 +1,38 @@
+;;; indiebrain-common.el --- Common functions used across my Emacs configuration  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Aaron
+
+;; Copyright (C) 2012-2022  Aaron Kuehler <aaron.kuehler@gmail.com>
+
+;; Author: Aaron <aaron.kuehler@gmail.com>
+;; Keywords: internal
+;; URL: https://github.com/indiebrain/.files/
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "28.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Common functions used across my Emacs configuration.
+;;
+;; See my full configuration: https://github.com/indiebrain/.files/
+
+;;; Code:
+
+
+(provide 'indiebrain-common)
+;;; indiebrain-common.el ends here
