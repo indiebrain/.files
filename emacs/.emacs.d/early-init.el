4b7fc06dd9e80c4e6dd33889ffccc1212626acac
Aaron Kuehler <aaron.kuehler@gmail.com>
2022-12-11 10:22:14 -0500

    Emacs: Tear down Emacs config

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,62 +1,69 @@
-;;; early-init.el --- Early Init File -*- lexical-binding: t -*-
+;;; early-init.el --- Emacs Early Initialization     -*- lexical-binding: t; -*-
 
-;; Copyright (c) 2012-2021  Aaron Kuehler <aaron.kuehler+public@gmail.com>
+;; Copyright (C) 2022  Aaron
 
-;; Author: Aaron Kuehler <aaron.kuehler+public@gmail.com>
-;; URL: <https://github.com/indiebrain/.files/>
+;; Author: Aaron <aaron.kuehler@gmail.com>
+;; Keywords: internal
+;; URL: https://github.com/indiebrain/.files/
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "28.0"))
+;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
-;; This file is free software: you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the
-;; Free Software Foundation, either version 3 of the License, or (at
-;; your option) any later version.
-;;
-;; This file is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
 ;; You should have received a copy of the GNU General Public License
-;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
-;; Prior to Emacs 27, the `init.el' was supposed to handle the
-;; initialisation of the package manager, by means of calling
-;; `package-initialize'.  Starting with Emacs 27, the default
-;; behaviour is to start the package manager before loading the init
-;; file.
+;; This configuration is loaded before Emacs loads the user's
+;; initialization file - 'init.el'. Items commonly found here are the
+;; default presentation settings, initialization of Emacs' package,
+;; configuration for Emacs' native compilation system, etc.
 ;;
-;; See my dotfiles: https://github.com/indiebrain/.files/
+;; See my full configuration: https://github.com/indiebrain/.files/
 
 ;;; Code:
 
-;; Initialize installed packages
-(setq package-enable-at-startup t)
+(setq frame-resize-pixelwise t)
 
-(defvar package-quickstart)
-
-;; Allow loading from the package cache
-(setq package-quickstart t)
-
-;; Do not resize the frame at this early stage.
+;; Prevent frame resizing during initialization
 (setq frame-inhibit-implied-resize t)
 
 ;; Disable GUI elements
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-splash-screen t)
-(setq use-dialog-box t)                 ; only for mouse events
-(setq use-file-dialog nil)
 
-(setq inhibit-startup-echo-area-message "indiebrain") ; read the docstring
+;; Set default frame size
+(dolist (var '(default-frame-alist initial-frame-alist))
+  (add-to-list var '(width . (text-pixels . 1200)))
+  (add-to-list var '(height . (text-pixels . 900))))
+
+;; Disable default "help"-ful messages
+(setq inhibit-splash-screen t)
+(setq use-dialog-box t) ; only for mouse events
+(setq use-file-dialog nil)
+(setq inhibit-startup-echo-area-message user-login-name) ; read the docstring
 (setq inhibit-startup-screen t)
 (setq inhibit-startup-buffer-menu t)
 
-(setq native-comp-async-report-warnings-errors 'silent) ; emacs28 with native compilation
 
+;; Initialize installed packages
+(setq package-enable-at-startup t)
+
+;; Allow package loading from the package cache
+(defvar package-quickstart)
+(setq package-quickstart t)
+
+(setq native-comp-async-report-warnings-errors 'silent)
 ;;; early-init.el ends here
