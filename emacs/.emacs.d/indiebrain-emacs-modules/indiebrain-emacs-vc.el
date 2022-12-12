baa1e360e533d8f79ec9399a697fb33cf78f9552
Aaron Kuehler <aaron.kuehler@gmail.com>
2022-12-11 21:53:44 -0500

    Emacs: Add version control configuration

---
+++ emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-vc.el
@@ -0,0 +1,227 @@
+;;; indiebrain-emacs-vc.el --- Configuration for version control, diff, etc  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2012-2022  Aaron Kuehler <aaron.kuehler@gmail.com>
+
+;; Author: Aaron <aaron.kuehler@gmail.com>
+;; URL: https://github.com/indiebrain/.files/
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "28.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Configuration for version control, diffs, etc.
+;;
+;; See my full configuration: https://github.com/indiebrain/.files/
+
+;;; Code:
+
+;;; Diff-mode (and indiebrain-diff.el extensions)
+(indiebrain-emacs-builtin-package 'diff-mode
+  (setq diff-default-read-only t)
+  (setq diff-advance-after-apply-hunk t)
+  (setq diff-update-on-the-fly t)
+  ;; The following are from Emacs 27.1
+  (setq diff-refine nil) ; I do it on demand, with my `agitate' package (more below)
+  (setq diff-font-lock-prettify t) ; I think nil is better for patches, but let me try this for a while
+  (setq diff-font-lock-syntax 'hunk-also)
+  (let ((map diff-mode-map))
+    (define-key map (kbd "L") #'vc-print-root-log)
+    ;; Emacs 29 can use C-x v v in diff buffers, which is great, but now I
+    ;; need quick access to it...
+    (define-key map (kbd "v") #'vc-next-action)))
+
+;;; Version control framework (vc.el and indiebrain-vc.el)
+(indiebrain-emacs-builtin-package 'vc
+  ;; Those offer various types of functionality, such as blaming,
+  ;; viewing logs, showing a dedicated buffer with changes to affected
+  ;; files.
+  (require 'vc-annotate)
+  (require 'vc-dir)
+  (require 'vc-git)
+  (require 'add-log)
+  (require 'log-view)
+
+  ;; This one is for editing commit messages.
+  (require 'log-edit)
+  (setq log-edit-confirm 'changed)
+  (setq log-edit-keep-buffer nil)
+  (setq log-edit-require-final-newline t)
+  (setq log-edit-setup-add-author nil)
+  ;; I can see the files from the Diff with C-c C-d
+  (remove-hook 'log-edit-hook #'log-edit-show-files)
+
+  (setq vc-find-revision-no-save t)
+  (setq vc-annotate-display-mode 'scale) ; scale to oldest
+  ;; I use a different account for git commits
+  (setq add-log-mailing-address "info@indiebrainesilaos.com")
+  (setq add-log-keep-changes-together t)
+  (setq vc-git-diff-switches '("--patch-with-stat" "--histogram"))
+  (setq vc-git-print-log-follow t)
+  (setq vc-git-revision-complete-only-branches nil) ; Emacs 28
+  (setq vc-git-root-log-format
+        `("%d %h %ai %an: %s"
+          ;; The first shy group matches the characters drawn by --graph.
+          ;; We use numbered groups because `log-view-message-re' wants the
+          ;; revision number to be group 1.
+          ,(concat "^\\(?:[*/\\|]+\\)\\(?:[*/\\| ]+\\)?"
+                   "\\(?2: ([^)]+) \\)?\\(?1:[0-9a-z]+\\) "
+                   "\\(?4:[0-9]\\{4\\}-[0-9-]\\{4\\}[0-9\s+:-]\\{16\\}\\) "
+                   "\\(?3:.*?\\):")
+          ((1 'log-view-message)
+           (2 'change-log-list nil lax)
+           (3 'change-log-name)
+           (4 'change-log-date))))
+
+  ;; These two are from Emacs 29
+  (setq vc-git-log-edit-summary-target-len 50)
+  (setq vc-git-log-edit-summary-max-len 70)
+
+  (setq vc-follow-symlinks t)
+
+  ;; NOTE: I override lots of the defaults
+  (let ((map global-map))
+    (define-key map (kbd "C-x v B") #'vc-annotate) ; Blame mnemonic
+    (define-key map (kbd "C-x v e") #'vc-ediff)
+    (define-key map (kbd "C-x v k") #'vc-delete-file) ; 'k' for kill==>delete is more common
+    (define-key map (kbd "C-x v G") #'vc-log-search)  ; git log --grep
+    (define-key map (kbd "C-x v t") #'vc-create-tag)
+    (define-key map (kbd "C-x v d") #'vc-diff))
+  (let ((map vc-dir-mode-map))
+    (define-key map (kbd "t") #'vc-create-tag)
+    (define-key map (kbd "O") #'vc-log-outgoing)
+    (define-key map (kbd "o") #'vc-dir-find-file-other-window)
+    (define-key map (kbd "d") #'vc-diff)         ; parallel to D: `vc-root-diff'
+    (define-key map (kbd "k") #'vc-dir-delete-file)
+    (define-key map (kbd "G") #'vc-revert))
+  (let ((map vc-git-stash-shared-map))
+    (define-key map "a" 'vc-git-stash-apply-at-point)
+    (define-key map "c" 'vc-git-stash) ; "create" named stash
+    (define-key map "k" 'vc-git-stash-delete-at-point) ; symmetry with `vc-dir-delete-file'
+    (define-key map "p" 'vc-git-stash-pop-at-point)
+    (define-key map "s" 'vc-git-stash-snapshot))
+  (let ((map vc-annotate-mode-map))
+    (define-key map (kbd "M-q") #'vc-annotate-toggle-annotation-visibility)
+    (define-key map (kbd "C-c C-c") #'vc-annotate-goto-line)
+    (define-key map (kbd "<return>") #'vc-annotate-find-revision-at-line))
+  (let ((map log-edit-mode-map))
+    (define-key map (kbd "M-s") nil) ; I use M-s for my search commands
+    (define-key map (kbd "M-r") nil)) ; I use `consult-history'
+  (let ((map log-view-mode-map))
+    (define-key map (kbd "<tab>") #'log-view-toggle-entry-display)
+    (define-key map (kbd "<return>") #'log-view-find-revision)
+    (define-key map (kbd "s") #'vc-log-search)
+    (define-key map (kbd "o") #'vc-log-outgoing)
+    (define-key map (kbd "f") #'vc-log-incoming)
+    (define-key map (kbd "F") #'vc-update)
+    (define-key map (kbd "P") #'vc-push)))
+
+;;; Agitate - A complement to vc-mode by Prot
+;; Read the manual here: <https://protesilaos.com/emacs/agitate>.
+(indiebrain-emacs-elpa-package 'agitate
+  (add-hook 'diff-mode-hook #'agitate-diff-enable-outline-minor-mode)
+  (advice-add #'vc-git-push :override #'agitate-vc-git-push-prompt-for-remote)
+
+  (setq agitate-log-edit-informative-show-root-log t)
+
+  (agitate-log-edit-informative-mode 1)
+
+  (let ((map global-map))
+    (define-key map (kbd "C-x v =") #'agitate-diff-buffer-or-file) ; replace `vc-diff'
+    (define-key map (kbd "C-x v g") #'agitate-vc-git-grep) ; replace `vc-annotate'
+    (define-key map (kbd "C-x v f") #'agitate-vc-git-find-revision)
+    (define-key map (kbd "C-x v s") #'agitate-vc-git-show)
+    (define-key map (kbd "C-x v w") #'agitate-vc-git-kill-commit-message)
+    (define-key map (kbd "C-x v p p") #'agitate-vc-git-format-patch-single)
+    (define-key map (kbd "C-x v p n") #'agitate-vc-git-format-patch-n-from-head))
+  (let ((map diff-mode-map))
+    (define-key map (kbd "C-c C-b") #'agitate-diff-refine-cycle) ; replace `diff-refine-hunk'
+    (define-key map (kbd "C-c C-n") #'agitate-diff-narrow-dwim))
+  (let ((map log-view-mode-map))
+    (define-key map (kbd "w") #'agitate-log-view-kill-revision)
+    (define-key map (kbd "W") #'agitate-log-view-kill-revision-expanded))
+  (let ((map vc-git-log-view-mode-map))
+    (define-key map (kbd "c") #'agitate-vc-git-format-patch-single))
+  (let ((map log-edit-mode-map))
+    (define-key map (kbd "C-c C-i C-n") #'agitate-log-edit-insert-file-name)
+    ;; See user options `agitate-log-edit-emoji-collection' and
+    ;; `agitate-log-edit-conventional-commits-collection'.
+    (define-key map (kbd "C-c C-i C-e") #'agitate-log-edit-emoji-commit)
+    (define-key map (kbd "C-c C-i C-c") #'agitate-log-edit-conventional-commit)))
+
+;;; Interactive and powerful git front-end (Magit)
+
+;; There is no need to install the package, as transient.el is built
+;; into Emacs.  By requiring it, I prevent the installation of the
+;; package, which would be done by Magit.
+(indiebrain-emacs-builtin-package 'transient)
+
+(indiebrain-emacs-elpa-package 'magit
+  (setq magit-define-global-key-bindings nil)
+  (define-key global-map (kbd "C-c g") #'magit-status)
+
+  (require 'git-commit)
+  (setq git-commit-summary-max-length 50)
+  (setq git-commit-known-pseudo-headers
+        '("Signed-off-by"
+          "Acked-by"
+          "Modified-by"
+          "Cc"
+          "Suggested-by"
+          "Reported-by"
+          "Tested-by"
+          "Reviewed-by"))
+  (setq git-commit-style-convention-checks
+        '(non-empty-second-line
+          overlong-summary-line))
+
+  (require 'magit-diff)
+  (setq magit-diff-refine-hunk t)
+
+  (require 'magit-repos)
+  (setq magit-repository-directories
+        '(("~/Git/Projects" . 1))))
+
+;;; Smerge and Ediff
+(indiebrain-emacs-builtin-package 'smerge-mode)
+
+(indiebrain-emacs-builtin-package 'ediff
+  (setq ediff-keep-variants nil)
+  (setq ediff-make-buffers-readonly-at-startup nil)
+  (setq ediff-merge-revisions-with-ancestor t)
+  (setq ediff-show-clashes-only t)
+  (setq ediff-split-window-function 'split-window-horizontally)
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
+  ;; Tweak those for safer identification and removal
+  (setq ediff-combination-pattern
+        '("<<<<<<< indiebrain-ediff-combine Variant A" A
+          ">>>>>>> indiebrain-ediff-combine Variant B" B
+          "####### indiebrain-ediff-combine Ancestor" Ancestor
+          "======= indiebrain-ediff-combine End"))
+
+  ;; TODO automate process in a robust way, or at least offer a good key
+  ;; binding.
+  (defun indiebrain/ediff-flush-combination-pattern ()
+    "Remove my custom `ediff-combination-pattern' markers.
+This is a quick-and-dirty way to get rid of the markers that are
+left behind by `smerge-ediff' when combining the output of two
+diffs.  While this could be automated via a hook, I am not yet
+sure this is a good approach."
+    (interactive)
+    (flush-lines ".*indiebrain-ediff.*" (point-min) (point-max) nil)))
+
+(provide 'indiebrain-emacs-vc)
+;;; indiebrain-emacs-vc.el ends here
