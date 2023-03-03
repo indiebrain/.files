3a50f6c3dfbad665cbf0afcffcddab5536898313
Aaron Kuehler <aaron.kuehler@gmail.com>
2023-03-03 09:55:05 -0500

    Emacs: Fix modus-theme loading

--- emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-modus-themes.el
+++ emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-modus-themes.el
@@ -147,20 +147,7 @@
           (5 . (rainbow (height 1.1)))
           (t . (variable-pitch extrabold))))
 
-  ;; Load the theme files before enabling a theme (else you get an error).
-  (modus-themes-load-themes)
-
-  ;; A simple check to load the desired theme at startup based on what
-  ;; the global preference for GNOME is. If such preference is not
-  ;; registered, it just loads `modus-operandi'. Check my dotfiles for
-  ;; the shell script called "delight", which handles system-wide theme
-  ;; switching (as I bind the `modus-themes-toggle' to <f5>, delight is
-  ;; bound to s-<f5> in the desktop's own custom key bindings).
-  (if (string-match-p
-       "dark"
-       (shell-command-to-string "gsettings get org.gnome.desktop.interface color-scheme"))
-      (modus-themes-load-vivendi)
-    (modus-themes-load-operandi))
+  (modus-themes-load-theme 'modus-vivendi)
 
   (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
 
