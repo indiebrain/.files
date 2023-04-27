01e220ed676b009982c6458f29d0e83cf8c8cebd
Aaron Kuehler <aaron.kuehler@gmail.com>
2023-04-27 16:27:03 -0400

    emacs-tab: Remove tab config

--- emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-window.el
+++ emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-window.el
@@ -39,72 +39,72 @@
 ;;; Window rules and basic tweaks (window.el)
 (indiebrain-emacs-builtin-package 'window
   (setq display-buffer-alist
-        `(;; no window
-          ("\\`\\*Async Shell Command\\*\\'"
-           (display-buffer-no-window))
-          ;; bottom side window
-          ("\\*Org Select\\*" ; the `org-capture' key selection
-           (display-buffer-in-side-window)
-           (dedicated . t)
-           (side . bottom)
-           (slot . 0)
-           (window-parameters . ((mode-line-format . none))))
-          ;; bottom buffer (NOT side window)
-          ((or . ((derived-mode . flymake-diagnostics-buffer-mode)
-                  (derived-mode . flymake-project-diagnostics-mode)
-                  (derived-mode . messages-buffer-mode)
-                  (derived-mode . backtrace-mode)
-                  "\\*\\(Warnings\\|Compile-Log\\)\\*"
-                  "\\*world-clock.*"))
-           (display-buffer-reuse-mode-window display-buffer-at-bottom)
-           (window-height . 0.3)
-           (dedicated . t)
-           (preserve-size . (t . t)))
-          ("\\*Embark Actions\\*"
-           (display-buffer-reuse-mode-window display-buffer-at-bottom)
-           (window-height . fit-window-to-buffer)
-           (window-parameters . ((no-other-window . t)
-                                 (mode-line-format . none))))
-          ("\\*\\(Output\\|Register Preview\\).*"
-           (display-buffer-reuse-mode-window display-buffer-at-bottom))
-          ;; below current window
-          ((derived-mode . help-mode) ; See the hooks for `visual-line-mode'
-           (display-buffer-reuse-mode-window display-buffer-below-selected))
-          ("\\*\\vc-\\(incoming\\|outgoing\\|git : \\).*"
-           (display-buffer-reuse-mode-window display-buffer-below-selected)
-           (window-height . 0.1)
-           (dedicated . t)
-           (preserve-size . (t . t)))
-          ((derived-mode . log-view-mode)
-           (display-buffer-reuse-mode-window display-buffer-below-selected)
-           (window-height . 0.3)
-           (preserve-size . (t . t)))
-          ((derived-mode . reb-mode) ; M-x re-builder
-           (display-buffer-reuse-mode-window display-buffer-below-selected)
-           (window-height . 4) ; note this is literal lines, not relative
-           (dedicated . t)
-           (preserve-size . (t . t)))
-          ("\\*\\(Calendar\\|Bookmark Annotation\\).*"
-           (display-buffer-reuse-mode-window display-buffer-below-selected)
-           (window-height . fit-window-to-buffer))
-          ;; NOTE 2022-09-10: The following is for `ispell-word', though
-          ;; it only works because I override `ispell-display-buffer'
-          ;; with `indiebrain-spell-ispell-display-buffer' and change the
-          ;; value of `ispell-choices-buffer'.  Check my indiebrain-spell.el
-          ;; for the details.
-          ("\\*ispell-top-choices\\*"
-           (display-buffer-reuse-mode-window display-buffer-below-selected)
-           (window-height . fit-window-to-buffer))
-          ;; new frame
-          ((or . ((derived-mode . Man-mode)
-                  (derived-mode . woman-mode)
-                  "\\*\\(Man\\|woman\\).*"))
-           (display-buffer-reuse-window display-buffer-pop-up-frame)
-           (pop-up-frame-parameters . ((width . (text-pixels . 640))
-                                       (height . (text-pixels . 360)))))
-          ;; same window
-          (indiebrain/display-buffer-shell-or-term-p ; see definition below
-           (display-buffer-reuse-window display-buffer-same-window))))
+	`(;; no window
+	  ("\\`\\*Async Shell Command\\*\\'"
+	   (display-buffer-no-window))
+	  ;; bottom side window
+	  ("\\*Org Select\\*" ; the `org-capture' key selection
+	   (display-buffer-in-side-window)
+	   (dedicated . t)
+	   (side . bottom)
+	   (slot . 0)
+	   (window-parameters . ((mode-line-format . none))))
+	  ;; bottom buffer (NOT side window)
+	  ((or . ((derived-mode . flymake-diagnostics-buffer-mode)
+		  (derived-mode . flymake-project-diagnostics-mode)
+		  (derived-mode . messages-buffer-mode)
+		  (derived-mode . backtrace-mode)
+		  "\\*\\(Warnings\\|Compile-Log\\)\\*"
+		  "\\*world-clock.*"))
+	   (display-buffer-reuse-mode-window display-buffer-at-bottom)
+	   (window-height . 0.3)
+	   (dedicated . t)
+	   (preserve-size . (t . t)))
+	  ("\\*Embark Actions\\*"
+	   (display-buffer-reuse-mode-window display-buffer-at-bottom)
+	   (window-height . fit-window-to-buffer)
+	   (window-parameters . ((no-other-window . t)
+				 (mode-line-format . none))))
+	  ("\\*\\(Output\\|Register Preview\\).*"
+	   (display-buffer-reuse-mode-window display-buffer-at-bottom))
+	  ;; below current window
+	  ((derived-mode . help-mode) ; See the hooks for `visual-line-mode'
+	   (display-buffer-reuse-mode-window display-buffer-below-selected))
+	  ("\\*\\vc-\\(incoming\\|outgoing\\|git : \\).*"
+	   (display-buffer-reuse-mode-window display-buffer-below-selected)
+	   (window-height . 0.1)
+	   (dedicated . t)
+	   (preserve-size . (t . t)))
+	  ((derived-mode . log-view-mode)
+	   (display-buffer-reuse-mode-window display-buffer-below-selected)
+	   (window-height . 0.3)
+	   (preserve-size . (t . t)))
+	  ((derived-mode . reb-mode) ; M-x re-builder
+	   (display-buffer-reuse-mode-window display-buffer-below-selected)
+	   (window-height . 4) ; note this is literal lines, not relative
+	   (dedicated . t)
+	   (preserve-size . (t . t)))
+	  ("\\*\\(Calendar\\|Bookmark Annotation\\).*"
+	   (display-buffer-reuse-mode-window display-buffer-below-selected)
+	   (window-height . fit-window-to-buffer))
+	  ;; NOTE 2022-09-10: The following is for `ispell-word', though
+	  ;; it only works because I override `ispell-display-buffer'
+	  ;; with `indiebrain-spell-ispell-display-buffer' and change the
+	  ;; value of `ispell-choices-buffer'.  Check my indiebrain-spell.el
+	  ;; for the details.
+	  ("\\*ispell-top-choices\\*"
+	   (display-buffer-reuse-mode-window display-buffer-below-selected)
+	   (window-height . fit-window-to-buffer))
+	  ;; new frame
+	  ((or . ((derived-mode . Man-mode)
+		  (derived-mode . woman-mode)
+		  "\\*\\(Man\\|woman\\).*"))
+	   (display-buffer-reuse-window display-buffer-pop-up-frame)
+	   (pop-up-frame-parameters . ((width . (text-pixels . 640))
+				       (height . (text-pixels . 360)))))
+	  ;; same window
+	  (indiebrain/display-buffer-shell-or-term-p ; see definition below
+	   (display-buffer-reuse-window display-buffer-same-window))))
 
   (defun indiebrain/display-buffer-shell-or-term-p (buffer &rest _)
     "Check if BUFFER is a shell or terminal.
@@ -112,9 +112,9 @@ This is a predicate function for `buffer-match-p', intended for
 use in `display-buffer-alist'."
     (when (string-match-p "\\*.*\\(e?shell\\|v?term\\).*" (buffer-name (get-buffer buffer)))
       (with-current-buffer buffer
-        ;; REVIEW 2022-07-14: Is this robust?
-        (and (not (derived-mode-p 'message-mode 'text-mode))
-             (derived-mode-p 'eshell-mode 'shell-mode 'comint-mode 'fundamental-mode)))))
+	;; REVIEW 2022-07-14: Is this robust?
+	(and (not (derived-mode-p 'message-mode 'text-mode))
+	     (derived-mode-p 'eshell-mode 'shell-mode 'comint-mode 'fundamental-mode)))))
 
   (setq window-combination-resize t)
   (setq even-window-sizes 'height-only)
@@ -155,40 +155,6 @@ use in `display-buffer-alist'."
     (define-key map (kbd "C-M-S-<right>") #'windmove-swap-states-right) ; conflicts with `org-increase-number-at-point'
     (define-key map (kbd "C-M-S-<down>") #'windmove-swap-states-down)
     (define-key map (kbd "C-M-S-<left>") #'windmove-swap-states-left)))
-
-;;; Tabs for window layouts (tab-bar.el and indiebrain-tab.el)
-(indiebrain-emacs-builtin-package 'tab-bar
-  (setq tab-bar-close-button-show nil)
-  (setq tab-bar-close-last-tab-choice nil)
-  (setq tab-bar-close-tab-select 'recent)
-  (setq tab-bar-new-tab-choice t)
-  (setq tab-bar-new-tab-to 'right)
-  (setq tab-bar-position nil)
-  (setq tab-bar-show nil)
-  (setq tab-bar-tab-hints nil)
-  (setq tab-bar-tab-name-function 'tab-bar-tab-name-current)
-  (setq tab-bar-auto-width-max '(120 . 20)) ; Emacs 29
-
-  (tab-bar-mode -1)                     ; see `indiebrain-tab-status-line'
-
-  ;; Same concept as `winner-mode'.  See the `indiebrain-tab-winner-undo' and
-  ;; its counterpart.
-  (tab-bar-history-mode 1))
-
-(indiebrain-emacs-builtin-package 'indiebrain-tab
-  (setq indiebrain-tab-tab-select-num-threshold 3)
-  (setq tab-bar-format                    ; Emacs 28
-        '(tab-bar-format-tabs-groups
-          tab-bar-format-align-right
-          tab-bar-format-global))
-
-  (add-hook 'after-init-hook #'indiebrain-tab-status-line)
-
-  (let ((map global-map))
-    (define-key map (kbd "C-x <right>") #'indiebrain-tab-winner-redo)
-    (define-key map (kbd "C-x <left>") #'indiebrain-tab-winner-undo)
-    (define-key map (kbd "<f8>") #'indiebrain-tab-status-line) ; unopinionated alternative: `indiebrain-tab-bar-toggle'
-    (define-key map (kbd "C-x t t") #'indiebrain-tab-select-tab-dwim)))
 
 (provide 'indiebrain-emacs-window)
 ;;; indiebrain-emacs-window.el ends here
