93b2f53520d81827cc91604cfefe7adbb50fa551
Aaron Kuehler <aaron.kuehler@gmail.com>
2023-07-04 11:50:34 -0400

    Emacs: Add dired-preview package

--- emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-dired.el
+++ emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-dired.el
@@ -128,6 +128,20 @@
   (setq trashed-sort-key '("Date deleted" . t))
   (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
 
+;;; Automatically preview Dired file at point (dired-preview.el)
+(indiebrain-emacs-package dired-preview
+  (:install "https://git.sr.ht/~protesilaos/dired-preview")
+  ;; These are all set to their default values.  I keep them here for
+  ;; reference.
+  (setq dired-preview-max-size (expt 2 20))
+  (setq dired-preview-ignored-extensions-regexp
+        (concat "\\."
+                "\\(mkv\\|" "webm\\|" "mp4\\|" "mp3\\|" "ogg\\|" "m4a\\|"
+                "gz\\|" "zst\\|" "tar\\|" "xz\\|" "rar\\|" "zip\\|"
+                "iso\\|" "epub\\|" "pdf\\)"))
+  (setq dired-preview-delay 0.7)
+  (add-hook 'dired-mode-hook #'dired-preview-mode))
+
 ;;; Ibuffer (dired-like buffer list manager)
 (indiebrain-emacs-package ibuffer
   (setq ibuffer-expert t)
