2caf89b13bc91448c193e0122e2466c4a52a7350
Aaron Kuehler <aaron.kuehler@gmail.com>
2024-03-26 11:34:29 -0400

    Emacs: Add tree-sitter grammars list

--- emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-langs.el
+++ emacs/.emacs.d/indiebrain-emacs-modules/indiebrain-emacs-langs.el
@@ -228,6 +228,28 @@
   (:install t)
   (define-key lsp-mode-map [remap xref-find-apropos] #'consult-lsp-symbols))
 
+;;; Tree-sitter
+(indiebrain-emacs-package tree-sitter
+  (:install t)
+
+  (setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
+
 ;;; Language specific settings
 
 ;;;; Caddy server (caddyfile-mode)
